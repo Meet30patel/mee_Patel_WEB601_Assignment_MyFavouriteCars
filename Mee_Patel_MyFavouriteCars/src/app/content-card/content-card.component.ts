195736b4f4f0b97aa94986d7c06d588fd54c139f
Meet30patel <113566773+Meet30patel@users.noreply.github.com>
2023-05-21 15:47:48 -0400

    first commit

---
+++ Mee_Patel_MyFavouriteCars/src/app/content-card/content-card.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { ContentList } from '../helper-files/content-list';
+
+
+@Component({
+  selector: 'app-content-card',
+  templateUrl: './content-card.component.html',
+  styleUrls: ['./content-card.component.scss']
+})
+export class ContentCardComponent  {
+  contentList: ContentList;
+
+  constructor() {
+    this.contentList = new ContentList();
+    this.contentList.addContent({
+      id: 1,
+      title: 'BMW',
+      description: 'Car',
+      creator: 'BMW',
+      imgURL: '',
+      type: 'Car',
+      tags: ['1,2']
+    });
+   
+  }
+}
